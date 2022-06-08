7d6f08d791251c30148662b13840148c2dca923c
Łukasz Kamiński <rolowaty@gmail.com>
2022-06-08 23:34:32 +0200

    sort

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    HOST: "cluster0.uyld5.mongodb.net",
+    LOGIN: "abase",
+    PASSWORD: "123"
+}
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
-const Users = require("./Users")
 
-mongoose.connect("mongodb+srv://abase:1234@cluster0.uyld5.mongodb.net/?retryWrites=true&w=majority", () => {
+const Users = require("./models/Users")
+const Role = require("./models/Role")
+
+const database = require('./config/db')
+
+mongoose.connect(`mongodb+srv://${database.LOGIN}:${database.PASSWORD}@${database.HOST}/?retryWrites=true&w=majority`, () => {
     console.log('Connected')
 })
-
-const user = new Users({
-    username: "Łukasz",
-    password: "123"
-})
-
-user.save().then(() => console.log('User Saved'))
